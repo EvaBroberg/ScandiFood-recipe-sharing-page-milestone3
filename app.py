f4a06de3da0e1a86acf9fba88230f7c9fd3b2bf1
Ieva <ievabroberg@gmail.com>
2019-11-10 15:18:04 +0000

    added wireftames for desktop view

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cookbook.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'python '
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
-class Cookbook(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    content = db.Column(db.string(500), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-@app.route('/home')
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/user/<name>')
-def user(name):
-    return 'User: {}'.format(name)    
+@app.route('/about')
+def about():
+    return render_template('about.html')    
 
-if __name__ == "__main__":
-    app.run(debug=True)    
+
+    if __name__ == '__main__':
+        app.run(debug=True)
