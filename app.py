6bdd62557e1b56d0de17ed9398141f186e41782c
Ieva <ievabroberg@gmail.com>
2019-11-15 21:57:42 +0000

    started working on login page functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -9,7 +9,16 @@ def index():
 @app.route('/recipes')
 def recipes():
     return render_template('recipes.html') 
-            
+
+@app.route('/login', methods=['GET','POST'])   
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid credentials. Please try again.'
+        else:
+            return redirect(url_for('index'))
+    return render_template('login.html',error =error)               
 
 if __name__ == '__main__':
     app.run(debug=True)
