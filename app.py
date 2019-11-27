09ca35751f5c9f8020a6c34ac398e943515fe2e1
Ieva <ievabroberg@gmail.com>
2019-11-27 09:53:52 +0000

    updated slider in home page, added uploads page

--- app.py
+++ app.py
@@ -1,31 +1,58 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import SubmitField
 from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
-# import sqlite3
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
 app.secret_key = "super secret key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['UPLOADED_PHOTOS_DEST'] = os.path.join(basedir, 'uploads')
 
 #create sqlalchemy object
 db = SQLAlchemy(app)
 
+
 from models import *
 
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
+patch_request_class(app)
 
-def login_required(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return f(*args, **kwargs)
-        else:
-            flash('Please log in')
-            return redirect(url_for('login'))
-    return wrap
+
+class UploadForm(FlaskForm):
+    photo = FileField(validators=[FileAllowed(photos, 'Image only!'), FileRequired('File was empty!')])
+    submit = SubmitField('Upload')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    form = UploadForm()
+    if form.validate_on_submit():
+        filename = photos.save(form.photo.data)
+        file_url = photos.url(filename)
+    else:
+        file_url = None
+    return render_template('upload.html', form=form, file_url=file_url)
+
+
+# def login_required(f):
+#     @wraps(f)
+#     def wrap(*args, **kwargs):
+#         if 'logged_in' in session:
+#             return f(*args, **kwargs)
+#         else:
+#             flash('Please log in')
+#             return redirect(url_for('login'))
+#     return wrap
 
 @app.route('/')
-@login_required
+# @login_required
 def index():
     return render_template('index.html') 
 
@@ -47,7 +74,7 @@ def login():
     return render_template('login.html',error =error) 
 
 @app.route('/logout')
-@login_required
+# @login_required
 def logout():
     session.pop('logged_in', None)
     flash('You are logged out')
@@ -59,3 +86,14 @@ def logout():
 if __name__ == '__main__':
     app.run(debug=True)
 
+
+
+
+
+
+
+
+
+
+# enter in order to run site
+# set DATABASE_URL=sqlite:///posts.db
--- static/js/script.js
+++ static/js/script.js
@@ -1,26 +1,67 @@
-filterSelection("all")
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
+//Slideshow
 
-// Show filtered elements
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
+;(function() {
+
+  var slider = function() {
+    
+    var backImg = [];
+    backImg[0] = "static/img/slide1.jpg";
+    backImg[1] = "static/img/slide2.jpg";
+    backImg[2] = "static/img/slide3.jpg";
+    
+    var i = 0;
+    var x = (backImg.length) - 1;
+    var int = 3500;
+    
+    interval = setInterval(showNext, int); // hoist?
+    
+    var elements = {
+      slider: document.querySelector('.slider'),
+      btn: { 
+        left: document.querySelector('.btnLeft'),
+        right: document.querySelector('.btnRight')
+      }
     }
-  }
-}
+    
+    var startInterval = function() {
+       interval = setInterval(showNext, int);
+    }
+    
+    var stopInterval = function() {
+      clearInterval(interval);
+    }
+    
+    var attachEvents = function() {
+      elements.btn.left.onclick = function() { showPrevious(); };
+      elements.btn.right.onclick = function() {  showNext(); };
+      elements.slider.addEventListener("mouseenter", stopInterval);
+      elements.slider.addEventListener("mouseleave", startInterval);
+    };
+    
+    var changeImg = function() {
+      elements.slider.style.backgroundImage = 'url(' + backImg[i] + ')';
+    }
+    
+    var initialize = (function() {
+      attachEvents();
+      changeImg(i);
+    })();
+
+    var showPrevious = function() {
+      (i <= 0) ? i = 3 : i--;
+      changeImg(i);
+    };
+
+    var showNext = function() {
+      (i >= x) ? i = 0 : i++;
+      changeImg(i);
+    };
+
+  };
+
+  slider();
+
+})();
 
 // Hide elements that are not selected
 function w3RemoveClass(element, name) {
@@ -42,6 +83,6 @@ for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+    this.className += "active";
   });
 }
