59548baebcc34c6ba7d23bd055b800c7253d279b
Ieva <ievabroberg@gmail.com>
2019-12-01 19:04:01 +0000

    created folders for organising, uploads users and recipes content, preparing to migrate all the code to these files

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ import os
 #default config
 class BaseConfig(object):
     DEBUG = False
-    SECRET_KEY = "super secret key"
+    #mihht need to delete b
+    SECRET_KEY = b"J/xb6R/x079/x8f/x02/xff/x84')/xaa X{/xf0"
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
