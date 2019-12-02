8bdae851180faf993f3144941c09b750bb5d5a51
Ieva <ievabroberg@gmail.com>
2019-12-02 22:09:54 +0000

    set a new database

--- scandiKitchen/__init__.py
+++ scandiKitchen/__init__.py
@@ -1,6 +1,20 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+#setup database
+basedir = os.path.abspath(os.path.dirname(__file__))
+#set up connection to database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+ os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK+MODIFICATIONS'] = False
+
+#create database
+db = SQLAlchemy(app)
+#connect app to database
+Migrate(app,db)
 
 from scandiKitchen.core.views import core
 app.register_blueprint(core)
---
+++ scandiKitchen/models.py
@@ -0,0 +1,8 @@
+from scandiKitchen import db
+
+class User(db.Model):
+    pass
+
+
+class Recipe(dn.model):
+    pass
